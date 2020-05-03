a918da30cc40c48c33de17cd444bf32087cd76cb
Kazi <alomk@mit.edu>
2020-05-03 00:49:07 -0400

    added timestamp to scanner

--- scan.py
+++ scan.py
@@ -1,9 +1,10 @@
 import json
 from subprocess import Popen, PIPE, STDOUT
+import datetime
 
-FILE_PATH = "./data-000000000000"
+FILE_PATH = "./data-000000000002"
 
-counter = 332 # what index to start at
+counter = 448 # what index to start at
 
 out = open(f"output{FILE_PATH[2:]}.txt","a")
 
@@ -11,7 +12,7 @@ with open(FILE_PATH, "r") as fp:
     lines = fp.readlines()
 
 for i in range(counter, len(lines), 1):
-    print(i)
+    print(str(i) + " " + str(datetime.datetime.now()))
     out.write(f"\nCOUNTER {counter}")
     p = Popen(['pakala', '-', '--exec-time','30', '--analysis-time', '30', '--max-transaction-depth', '5', '-z'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
     p_stdout = p.communicate(input=bytes(json.loads(lines[i])['bytecode'][2:], "UTF-8"))
